786b2d9f7408455d901cdc1ad8f870a466682855
nazanin akhtarian <n.akhtarian@gmail.com>
2020-06-17 14:04:32 +0430

    commit4

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from yekta.models import URL
 
 
 def register(request):
@@ -19,12 +19,46 @@ def register(request):
 	
 	return render(request, 'users/register.html', {'form': form})
 
+
+def encode(id):
+    # base 62 characters
+    characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    base = len(characters)
+    ret = []
+    # convert base10 id into base62 id for having alphanumeric shorten url
+    while id > 0:
+        val = id % base
+        ret.append(characters[val])
+        id = id // base
+    # since ret has reversed order of base62 id, reverse ret before return it
+    return "".join(ret[::-1])
+
+
 def main(request):
+	id = 10000000000 + len(URL.objects.all())
+
 	if request.method == 'POST':
-		return render(request, 'users/main.html', {'content': request.POST['userLink']})
-	else:    
-	    return render(request, 'users/main.html')
+		shorten_url = encode(id)
+		if 'save' in request.POST:
+			URL.objects.create(link="http://127.0.0.1:8000/shorter/" + shorten_url, numberOfVisits=0, numberOfUsers=0)
+		
+		
+		return render(request, 'users/main.html', {'content': "http://127.0.0.1:8000/shorter/" + shorten_url})
+	else:
+		return render(request, 'users/main.html')
+        # urls = URL.objects.all
+     #    context = {
+     #    # 'posts': URL.objects.all()
+    	# }
+        
+        # increase cnt for next url
+       	
+
 
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
+
+
+
+
--- yekta/models.py
+++ yekta/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class URL(models.Model):
 	link = models.TextField()
 	numberOfVisits = models.IntegerField()
 	numberOfUsers = models.IntegerField()
-		
+	
+
+# class User
